9e29d4f65f67e575c0ecff8735c9e9d4a95ed4b2
konokadam <53538563+konokadam@users.noreply.github.com>
2022-10-21 23:17:26 +0300

    Update module_multilinear_interpolation.f90

--- src/module_multilinear_interpolation.f90
+++ src/module_multilinear_interpolation.f90
@@ -1,6 +1,6 @@
 module precision
 
-    use, intrinsic :: iso_fortran_env, only: real32, real64, real128    !!!single, double, quadrable
+    use, intrinsic :: iso_fortran_env, only: real32, real64, real128
     implicit none
     private
     integer, parameter, public :: rp = real64                    !.. working precision for reals
