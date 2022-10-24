714580a817de09afdc0638bf7f242181e22402ef
konokadam <53538563+konokadam@users.noreply.github.com>
2022-10-24 20:48:51 +0300

    Update test.f90

--- test/test.f90
+++ test/test.f90
@@ -6,82 +6,50 @@ program test
     type(table_type) :: xt(3)
     real(rp), allocatable :: f(:)
     real(rp) :: x(3)
-    real(rp) :: value
+    real(rp) :: value, value_exact
+    integer :: i, j, k, icount
+    real(rp) :: coeffs(3) = [0.3_rp, 7._rp, 13._rp]
 
     allocate(xt(1)%params(5))
-    xt(1)%params = [13._rp, 17._rp, 23._rp, 29._rp, 31._rp]
+    xt(1)%params = [100._rp, 500._rp, 1000._rp, 1500._rp, 2000._rp]
     allocate(xt(2)%params(4))
-    xt(2)%params = [2._rp, 6._rp, 11._rp, 12._rp]
+    xt(2)%params = [15._rp, 25._rp, 35._rp, 45._rp]
     allocate(xt(3)%params(3))
     xt(3)%params = [7._rp, 9._rp, 11._rp]
 
     allocate(f(size(xt(1)%params)*size(xt(2)%params)*size(xt(3)%params)))
-    f(1) = 0.4754_rp
-    f(2) = 0.6112_rp
-    f(3) = 0.7471_rp
-    f(4) = 1.4263_rp
-    f(5) = 1.8338_rp
-    f(6) = 2.2413_rp
-    f(7) = 2.6149_rp
-    f(8) = 3.362_rp
-    f(9) = 4.1091_rp
-    f(10) = 2.8526_rp
-    f(11) = 3.6677_rp
-    f(12) = 4.4827_rp
-    f(13) = 0.6217_rp
-    f(14) = 0.7993_rp
-    f(15) = 0.977_rp
-    f(16) = 1.8652_rp
-    f(17) = 2.3981_rp
-    f(18) = 2.931_rp
-    f(19) = 3.4195_rp
-    f(20) = 4.3965_rp
-    f(21) = 5.3735_rp
-    f(22) = 3.7304_rp
-    f(23) = 4.7962_rp
-    f(24) = 5.862_rp
-    f(25) = 0.8411_rp
-    f(26) = 1.0815_rp
-    f(27) = 1.3218_rp
-    f(28) = 2.5235_rp
-    f(29) = 3.2445_rp
-    f(30) = 3.9655_rp
-    f(31) = 4.6264_rp
-    f(32) = 5.9482_rp
-    f(33) = 7.2701_rp
-    f(34) = 5.047_rp
-    f(35) = 6.489_rp
-    f(36) = 7.931_rp
-    f(37) = 1.0606_rp
-    f(38) = 1.3636_rp
-    f(39) = 1.6666_rp
-    f(40) = 3.1818_rp
-    f(41) = 4.0909_rp
-    f(42) = 5_rp
-    f(43) = 5.8333_rp
-    f(44) = 7.5_rp
-    f(45) = 9.1666_rp
-    f(46) = 6.3636_rp
-    f(47) = 8.1818_rp
-    f(48) = 10_rp
-    f(49) = 1.1337_rp
-    f(50) = 1.4576_rp
-    f(51) = 1.7816_rp
-    f(52) = 3.4012_rp
-    f(53) = 4.373_rp
-    f(54) = 5.3448_rp
-    f(55) = 6.2356_rp
-    f(56) = 8.0172_rp
-    f(57) = 9.7988_rp
-    f(58) = 6.8025_rp
-    f(59) = 8.746_rp
-    f(60) = 10.6896_rp
 
-    x = [34.39_rp, 2.75_rp, 1.37_rp]
-    write(*,*) "Input Params: ", x
+    !!!Write example table
+    open(1, file = "example_table_3param.dat ")
+    write(1,'(4a10)') "xt(1)", "xt(2)", "xt(3)", "f"
+    icount = 0
+    do i = 1, size(xt(1)%params)
+        do j = 1, size(xt(2)%params)
+            do k = 1, size(xt(3)%params)
+                icount = icount + 1
+                f(icount) = coeffs(1) * xt(1)%params(i) + &
+                            coeffs(2) * xt(2)%params(j) + &
+                            coeffs(3) * xt(3)%params(k)
+
+                write(1,'(4f10.4)') xt(1)%params(i), xt(2)%params(j), xt(3)%params(k), f(icount)
+            end do
+        end do
+    end do
+
+    close(1)
+
+    x = [575.39_rp, 39.72_rp, 7.27_rp]
+    write(*,'(1a25,3f10.2)') "Input Params: ", x
     write(*,*) "----------------------------------------------------"
     value = multilinear_interpolation(xt, f, x)
-    write(*,*) "Interpolated value: ", value
+    write(*,'(1a25,1f10.2)') "Interpolated value: ", value
+    value_exact = coeffs(1) * x(1) + coeffs(2) * x(2) + coeffs(3) * x(3)
+    write(*,'(1a25,1f10.2)') "Exact value: ", value_exact
+    write(*,'(1a25,1l10)') "Interpolation correct: ", abs(value_exact - value) < small
     write(*,*) "----------------------------------------------------"
+
+    do i = 1, size(xt)
+        deallocate(xt(i)%params)
+    end do
 
 end program
