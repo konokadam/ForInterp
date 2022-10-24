5b38522f38812e6c8cdcf1027d692b964b16981f
konokadam <53538563+konokadam@users.noreply.github.com>
2022-10-24 23:28:57 +0300

    Update module_multilinear_interpolation.f90

--- src/module_multilinear_interpolation.f90
+++ src/module_multilinear_interpolation.f90
@@ -1,10 +1,10 @@
 module precision
 
-    use, intrinsic :: iso_fortran_env, only: real32, real64, real128
+    use, intrinsic :: iso_fortran_env, only: real32, real64, real128    !!!single, double, quadrable
     implicit none
     private
     integer, parameter, public :: rp = real64                    !.. working precision for reals
-    real(rp), public :: small = 1.e-12_rp
+    real(rp), public :: small = 1.e-6_rp
 
 end module
 
@@ -17,10 +17,17 @@ module multilinear_interpolation_mod
     use :: precision
 
     implicit none
-    type :: table_type
+    type :: param_type
         real(rp), allocatable :: params(:)
     end type
 
+    type :: table_type
+        type(param_type), allocatable :: x(:)   !!!Table parameters
+        real(rp), allocatable :: f(:)           !!!Table line values
+    contains
+        procedure :: interpolation => multilinear_interpolation
+        final :: delete_table
+    end type
 
 contains
 
@@ -184,17 +191,31 @@ contains
 
     end subroutine dintrv
 
-    !!!Author H. Emrah Konokman
-    !! General, multi-linear interpolation function
-    !  If out of bounds then extrapolation is performed.
-    pure function multilinear_interpolation(xt, f, x) result(value)
+    subroutine delete_table(self)
 
-        type(table_type), intent(in) :: xt(:)     !!!Table parameters
-        real(rp), intent(in) :: f(:)              !!!Table line values
+        type(table_type) :: self
+
+        integer :: i
+
+        if(allocated(self%x)) then
+            do i = 1, size(self%x)
+                if(allocated(self%x(i)%params)) deallocate(self%x(i)%params)
+            end do
+            deallocate(self%x)
+        end if
+        if(allocated(self%f)) deallocate(self%f)
+
+    end subroutine
+
+    !!!Interpolation function
+    !! If input parameter is out of lower or upper limits then extrapolation is performed.
+    pure function multilinear_interpolation(self, x) result(value)
+
+        class(table_type), intent(in) :: self     !!!Table parameters
         real(rp), intent(in) :: x(:)              !!!Input parameters to interpolate
         real(rp) :: value                         !!!Interpolated value
 
-        integer :: ix(size(xt),2)
+        integer :: ix(size(self%x),2)
         integer :: mflag
         integer :: i, j, num_param, num_f
         integer :: indx, iopp
@@ -204,7 +225,7 @@ contains
         real(rp) :: sign
 
         num_param = size(x)
-        num_f = size(f)
+        num_f = size(self%f)
         sign = 1._rp
 
         !!!Contribution of the bound value is proportional to the opposite volume
@@ -220,7 +241,7 @@ contains
         !!!Find lower and upper bounds of the table
         do i = 1, num_param
             ilo = 1
-            call dintrv(xt(i)%params, x(i), ilo, ix(i,1), ix(i,2), mflag)
+            call dintrv(self%x(i)%params, x(i), ilo, ix(i,1), ix(i,2), mflag)
         end do
 
         !!!Determine bound and opposite bound indices and calculate opposite volumes
@@ -237,19 +258,19 @@ contains
                 !!!iopp: Index of ix (from dintrv) to get the table parameters to calculate opposite volume
                 iopp = 3 - indx
                 !!!Opposite volume of the bound
-                if(iopp == 1 .and. x(j) > xt(j)%params(ix(j,2))) sign = -1._rp
-                if(iopp == 2 .and. x(j) < xt(j)%params(ix(j,2))) sign = -1._rp
-                vol_opp = vol_opp * sign * abs(xt(j)%params(ix(j,iopp)) - x(j))
+                if(iopp == 1 .and. x(j) > self%x(j)%params(size(self%x(j)%params))) sign = -1._rp
+                if(iopp == 2 .and. x(j) < self%x(j)%params(1)) sign = -1._rp
+                vol_opp = vol_opp * sign * abs(self%x(j)%params(ix(j,iopp)) - x(j))
                 sign = 1._rp
                 !!!indexf: Index of the bound value (f)
-                fr = fr/size(xt(j)%params)
+                fr = fr/size(self%x(j)%params)
                 indexf = indexf + (ix(j,indx) - 1) * fr
             end do
 
             indexf = indexf + 1
 
             !!!Contribution of the bound value is proportional to opposite volume
-            value = value + vol_opp * f(indexf)
+            value = value + vol_opp * self%f(indexf)
             vol_opp_sum = vol_opp_sum + vol_opp
         end do
 
